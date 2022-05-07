65dcdec56ba4db3aad55a4f0b17880033176dd52
jackarms <90922918+jackarms@users.noreply.github.com>
2022-05-07 08:09:44 -0500

    initial layout created, files

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const numberOneEl = document.getElementById("1");
+const currentOutputEl = document.getElementsByClassName("current-operand");
+
+numberOneEl.addEventListener("click", function () {
+  console.log("You clicked the number 1");
+});
